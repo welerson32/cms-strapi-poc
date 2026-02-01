65a1566a4b14de6de982678eba5accb4fbc5a49b
Welerson Freitas <wfggames@hotmail.com>
2026-02-01 19:13:37 -0300

    feat: banners collection configuration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,21 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:4200'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'user-id'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
